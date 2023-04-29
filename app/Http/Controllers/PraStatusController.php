a5facf5184eddf3001f47d8ea350a8877c5c5063
kajal <kajal@shoplover.com>
2023-04-29 18:26:35 +0600

    asc id pra status

--- app/Http/Controllers/PraStatusController.php
+++ app/Http/Controllers/PraStatusController.php
@@ -19,7 +19,7 @@ class PraStatusController extends Controller
         if($request->filled('MAD_ROC_NO1')) $pra_status->where('employer_identification_card_no', $request->MAD_ROC_NO1);
         if($request->filled('MAD_ROC_NO2')) $pra_status->where('company_registration_no', $request->MAD_ROC_NO2);
         if($request->filled('MAD_APPL_NO')) $pra_status->where('application_no', $request->MAD_APPL_NO);
-        return view('pra_status.search', ['pra_status_list' => $pra_status->orderBy('id', 'desc')->paginate(6)]);
+        return view('pra_status.search', ['pra_status_list' => $pra_status->orderBy('id', 'asc')->paginate(6)]);
     }
 
 
